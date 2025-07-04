cc294af697e58f35d6e4209b66f46368e837ad3c
AriWidyaa <ariwidyabali@gmail.com>
2025-07-04 11:16:29 +0800

    OOP-UAS-C-2301010270

---
+++ ProjekUAS/src/oopuasc2301010270/ProjekUAS.java
@@ -0,0 +1,17 @@
+package oopuasc2301010270;
+
+/**
+ *
+ * @author Ari Widya
+ * TGL : 28 Juni 2025
+ */
+public class ProjekUAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
